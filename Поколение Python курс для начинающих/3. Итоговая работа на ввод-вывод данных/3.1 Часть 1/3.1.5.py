d24b21563bd8cc5165e4690ad4b2de9bd7c4b287
alni68 <72321089+alni68@users.noreply.github.com>
2023-04-07 16:08:40 +0400

    3.1 Часть 1
    3  Итоговая работа на ввод-вывод данных

---
+++ Поколение Python курс для начинающих/3. Итоговая работа на ввод-вывод данных/3.1 Часть 1/3.1.5.py
@@ -0,0 +1,9 @@
+'''
+Выберите верные строки кода.
+'''
+
+print("Told you not to worry", "But maybe that's a lie", sep=' :) ')
+
+print("Honey, what's your hurry", end='?')
+
+print("The world's a little blurry", "Or maybe it's my eyes", end='!!!', sep=' :) ')
